3a9a336384fe0818767a9ade68f89c5ae7c8c888
kellynooson <79423811+kellynooson@users.noreply.github.com>
2021-02-24 20:45:12 -0700

    Add files via upload

---
+++ homework 2.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	double a,b;
+	cout<<"Enter one number: ";
+	cin>>a;
+	cout<<"Enter second number: ";
+	cin>>b;
+	
+	cout<<a<<" + "<<b<<" = "<<a+b<<endl;
+	
+	double c,d;
+	cout<<"Enter one number: ";
+	cin>>c;
+	cout<<"Enter second number: ";
+	cin>>d;
+	
+	cout<<c<<" * "<<d<<" = "<<c*d<<endl;
+	
+	double e,f;
+	cout<<"Enter one number: ";
+	cin>>e;
+	cout<<"Enter second number: ";
+	cin>>f;
+	
+	cout<<e<<" / "<<f<<" = "<<e / f<<endl;
+	
+	double g,h;
+	cout<<"Enter one number: ";
+	cin>>g;
+	cout<<"Enter a second number: ";
+	cin>>h;
+	
+	cout<<g<<" - "<<h<<" = "<<g - h;
+		
+	
+	
+	
+	
+	return 0;
+}
