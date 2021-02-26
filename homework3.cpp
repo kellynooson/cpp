57f50e3f6ed118ba757632a277c0d9f524068de2
kellynooson <79423811+kellynooson@users.noreply.github.com>
2021-02-25 19:33:07 -0700

    Add files via upload

---
+++ homework3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	double a,b;
+	cout<<"Enter year of birth: ";
+	cin>>a;
+	cout<<"Enter what year it is now: ";
+	cin>>b;
+	
+	cout<<b<<" - "<<a<<" = "<<b-a;
+	
+	return 0;
+}
