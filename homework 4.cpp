20d6bb128deaf27616437e0c0823abb67a30f7e7
kellynooson <79423811+kellynooson@users.noreply.github.com>
2021-02-26 18:59:45 -0700

    Add files via upload

---
+++ homework 4.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	double a,b;
+	cout<<"Enter your original buying price: ";
+	cin>>a;
+	cout<<"Now enter your selling price of whatever you want(try to make it greater than buying price): ";
+	cin>>b;
+	
+	cout<<b<<" - "<<a<<" = "<<b-a;
+	
+	return 0;
+}
