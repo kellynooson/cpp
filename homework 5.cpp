5046fde2250d7f707b608d54862bd79f55f16c02
kellynooson <79423811+kellynooson@users.noreply.github.com>
2021-02-28 11:09:27 -0700

    Add files via upload

---
+++ homework 5.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	double a;
+	cout<<"Enter any number including negatives: ";
+	cin>>a;
+	
+	if(a<0){
+		cout<<"this number is a negative";
+	}else{
+		cout<<"this number is a positive";
+	}
+	
+	
+	return 0;
+}
